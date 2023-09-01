91c89463d902f3e7cdd741f335e40c425abcafcf
pyshadi <shadi.alhaj.ch@gmail.com>
2023-09-01 22:40:10 +0200

    updated main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from ws_parser import  WiresharkParser
 
 
-parser = WiresharkParser(r'C:\Users\soundation\source\ws_parser\data\capture')
+parser = WiresharkParser(r'..\data\capture')
 parser.parse_packets()
 parser.visualize_network_traffic()
 parser.visualize_network_traffic_table()
